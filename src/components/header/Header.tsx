b21c1e1627689ba0618c4e0b0eecbed6968ed973
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-05-11 01:58:54 -0300

    feat: view do feed nas versões desktop, tablet e móvel inicialmente definidas

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,81 @@
-import './style.css';
+import logo from "../../assets/moments.png";
+import "./style.css";
 
 function Header() {
   return (
-    <aside>
-          <header>
-              <a className="brand">
-                  <img src="" alt="Moments" />
-                  <h2>Moments</h2>
+    <>
+      <aside>
+        <header>
+          <a className="brand">
+            <img src={logo} alt="Moments" />
+            <h2>Moments</h2>
+          </a>
+        </header>
+        <nav>
+          <ul className="links">
+            <li>
+              <a href="#">
+                <i className="fa-solid fa-house" />
+                <span>Home</span>
               </a>
-          </header>
-          <nav>
-              <ul className="links">
-                  <li><a href="#"><span>Home</span></a></li>
-                  <li id="add"><a href="#">Novo</a></li>
-                  <li><a href="#">Sobre</a></li>
-              </ul>
-          </nav>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa-solid fa-champagne-glasses" />
+                <span>Postar</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa-solid fa-user"></i>
+                <span>Perfil</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa-solid fa-user-group"></i>
+                <span>Seguidores</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </aside>
-  )
+      <div className="mobile-header">
+        <a className="brand-mobile" href="#">
+          <img src={logo} alt="Moments" />
+          <h2>Moments</h2>
+        </a>
+      </div>
+      <nav className="footer-bar">
+        <ul className="foot-links">
+          <li>
+            <a href="#">
+              <i className="fa-solid fa-house" />
+              <span>Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa-solid fa-champagne-glasses" />
+              <span>Postar</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa-solid fa-user"></i>
+              <span>Perfil</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa-solid fa-user-group"></i>
+              <span>Seguidores</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
-export default Header
+export default Header;
